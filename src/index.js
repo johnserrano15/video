8b3b70aabeab12cfec001fb5541f5972d25c5f55
John serrano <andrey_201015@hotmail.com>
2018-02-13 11:02:51 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// console.log('Hola mundo');
+
+const app = document.getElementById('app');
+const holaMundo = <h1>Hola mundo React :)</h1>;
+
+ReactDOM.render(holaMundo, app);
