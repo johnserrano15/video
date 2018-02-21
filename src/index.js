53e4a017bd365b93da964d0833459dcea4e54fa0
John serrano <andrey_201015@hotmail.com>
2018-02-21 14:58:42 -0500

    Usando props

--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ const app = document.getElementById('app');
 const holaMundo = <h1>Hola mundo React :)</h1>;
 
 // ReactDOM.render(<Media />, app);
-render(<Media />, app);
+render(<Media title="¿Por qué aprender React?" author="John Serrano" image="./images/covers/bitcoin.jpg" />, app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -14,11 +14,11 @@ class Media extends Component {
       }
     }
     return (
-      <div style={styles.container} className="Media">
+      <div className="Media">
         <div className="Media-cover">
-          <img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160} />
-          <h3 className="Media-title">¿Por qué aprender React?</h3>
-          <p className="Media-author">John Serrano :D</p>
+          <img className="Media-image" src={this.props.image} alt="" width={260} height={160} />
+          <h3 className="Media-title">{this.props.title}</h3>
+          <p className="Media-author">{this.props.author}</p>
         </div>
       </div>
     )
